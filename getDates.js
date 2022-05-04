463514e9beb855c12f88247e00ab4779dd0e4a10
It'sa me Malario <69772002+Zoldyck99@users.noreply.github.com>
2022-05-04 14:54:40 +0300

    Add files via upload

--- getDates.js
+++ getDates.js
@@ -1,114 +1,124 @@
-function getDates(weekDay='', startDate, period=1, dateFormat=1, direction=1){
-            try {
-                let date = new Date(startDate);
-                let endMonth = new Date(startDate);
+/**
+ * 
+ * @param {String} weekDay 
+ * @param {String,Date} startDate 
+ * @param {Integer} period 
+ * @param {Integer} dateFormat 
+ * @param {Boolean} reverse 
+ * @returns array of date Strings
+ */
+function getDates(weekDay='', startDate, period=1, dateFormat=1, reverse=false){
+    try {
+        let date = new Date(startDate);
+        let endMonth = new Date(startDate);
 
-                const arr = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-                if(! arr.includes(weekDay)) {throw "Week day is not specified properly! should be one of: " + arr;}
-                while(date.getDay() != arr.indexOf(weekDay)) {date.setDate(date.getDate() + 1);}    
+        const arr = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+        if(! arr.includes(weekDay)) {throw "Week day is not specified properly! should be one of: " + arr;}
+        while(date.getDay() != arr.indexOf(weekDay)) {date.setDate(date.getDate() + 1);}    
 
-                let days = [];
-                let m;
-                let d;
-                let y;
+        let days = [];
+        let m;
+        let d;
+        let y;
 
-                if(direction == -1){    // passing dates
-                    endMonth.setMonth(date.getMonth() - period);  
-                    endMonth = endMonth.getMonth();
+        if(reverse){    // passing dates
+            endMonth.setMonth(date.getMonth() - period);  
+            endMonth = endMonth.getMonth();
 
-                    if(dateFormat == 1){
-                        while (date.getMonth() != endMonth) {
-                            m = date.getMonth() + 1;
-                            d = date.getDate();
-                            y = date.getFullYear();
-                    
-                            days.push(
-                            y + '-' +
-                            (m < 10 ? '0' + m : m) + '-' +
-                            (d < 10 ? '0' + d : d)
-                            );
-                            date.setDate(date.getDate() - 7);
-                        }
-                    }else if(dateFormat == 2){
-                        while (date.getMonth() != endMonth) {
-                            m = date.getMonth() + 1;
-                            d = date.getDate();
-                            y = date.getFullYear();
-                    
-                            days.push(
-                            y + '/' +
-                            (m < 10 ? '0' + m : m) + '/' +
-                            (d < 10 ? '0' + d : d)
-                            );
-                            date.setDate(date.getDate() - 7);
-                        }
-                    }else{
-                        while (date.getMonth() != endMonth) {
-                            m = date.toLocaleString('default', { month: 'short' });
-                            d = date.getDate();
-                            y = date.getFullYear();
-                    
-                            days.push(
-                            y + ' ' +
-                            (m < 10 ? '0' + m : m) + ' ' +
-                            (d < 10 ? '0' + d : d)
-                            );
-                            date.setDate(date.getDate() - 7);
-                        }
-                    }
-
-
-                }else{      // coming dates
-                    endMonth.setMonth(date.getMonth() + period);  
-                    endMonth = endMonth.getMonth() + 1;
-
-                    if(dateFormat == 1){
-                        while (date.getMonth() != endMonth) {
-                            m = date.getMonth() + 1;
-                            d = date.getDate();
-                            y = date.getFullYear();
-                    
-                            days.push(
-                            y + '-' +
-                            (m < 10 ? '0' + m : m) + '-' +
-                            (d < 10 ? '0' + d : d)
-                            );
-                            date.setDate(date.getDate() + 7);
-                        }
-                    }else if(dateFormat == 2){
-                        while (date.getMonth() != endMonth) {
-                            m = date.getMonth() + 1;
-                            d = date.getDate();
-                            y = date.getFullYear();
-                    
-                            days.push(
-                            y + '/' +
-                            (m < 10 ? '0' + m : m) + '/' +
-                            (d < 10 ? '0' + d : d)
-                            );
-                            date.setDate(date.getDate() + 7);
-                        }            
-                    }else{
-                        while (date.getMonth() != endMonth) {
-                            m = date.toLocaleString('default', { month: 'short' });
-                            d = date.getDate();
-                            y = date.getFullYear();
-                    
-                            days.push(
-                            y + ' ' +
-                            (m < 10 ? '0' + m : m) + ' ' +
-                            (d < 10 ? '0' + d : d)
-                            );
-                            date.setDate(date.getDate() + 7);
-                        }      
-                    }
-
+            if(dateFormat == 1){
+                while (date.getMonth() != endMonth) {
+                    m = date.getMonth() + 1;
+                    d = date.getDate();
+                    y = date.getFullYear();
+            
+                    days.push(
+                    y + '-' +
+                    (m < 10 ? '0' + m : m) + '-' +
+                    (d < 10 ? '0' + d : d)
+                    );
+                    date.setDate(date.getDate() - 7);
                 }
-
-
-                return days;
-            } catch (err) {
-                console.log(err)
+            }else if(dateFormat == 2){
+                while (date.getMonth() != endMonth) {
+                    m = date.getMonth() + 1;
+                    d = date.getDate();
+                    y = date.getFullYear();
+            
+                    days.push(
+                    y + '/' +
+                    (m < 10 ? '0' + m : m) + '/' +
+                    (d < 10 ? '0' + d : d)
+                    );
+                    date.setDate(date.getDate() - 7);
+                }
+            }else{
+                while (date.getMonth() != endMonth) {
+                    m = date.toLocaleString('default', { month: 'short' });
+                    d = date.getDate();
+                    y = date.getFullYear();
+            
+                    days.push(
+                    y + ' ' +
+                    (m < 10 ? '0' + m : m) + ' ' +
+                    (d < 10 ? '0' + d : d)
+                    );
+                    date.setDate(date.getDate() - 7);
+                }
             }
-          
+
+
+        }else{      // coming dates
+            endMonth.setMonth(date.getMonth() + period);  
+            endMonth = endMonth.getMonth() + 1;
+
+            if(dateFormat == 1){
+                while (date.getMonth() != endMonth) {
+                    m = date.getMonth() + 1;
+                    d = date.getDate();
+                    y = date.getFullYear();
+            
+                    days.push(
+                    y + '-' +
+                    (m < 10 ? '0' + m : m) + '-' +
+                    (d < 10 ? '0' + d : d)
+                    );
+                    date.setDate(date.getDate() + 7);
+                }
+            }else if(dateFormat == 2){
+                while (date.getMonth() != endMonth) {
+                    m = date.getMonth() + 1;
+                    d = date.getDate();
+                    y = date.getFullYear();
+            
+                    days.push(
+                    y + '/' +
+                    (m < 10 ? '0' + m : m) + '/' +
+                    (d < 10 ? '0' + d : d)
+                    );
+                    date.setDate(date.getDate() + 7);
+                }            
+            }else{
+                while (date.getMonth() != endMonth) {
+                    m = date.toLocaleString('default', { month: 'short' });
+                    d = date.getDate();
+                    y = date.getFullYear();
+            
+                    days.push(
+                    y + ' ' +
+                    (m < 10 ? '0' + m : m) + ' ' +
+                    (d < 10 ? '0' + d : d)
+                    );
+                    date.setDate(date.getDate() + 7);
+                }      
+            }
+
         }
+
+
+        return days;
+    } catch (err) {
+        console.log(err)
+    }          
+}
+
+export default getDates;
